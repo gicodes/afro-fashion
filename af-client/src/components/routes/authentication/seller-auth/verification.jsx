737717aad05729546de74bcf78345f910cb9bd63
Gideon, I <gicodes9@gmail.com>
2024-03-10 14:09:34 +0100

    isVerified

--- af-client/src/components/routes/authentication/seller-auth/verification.jsx
+++ af-client/src/components/routes/authentication/seller-auth/verification.jsx
@@ -5,7 +5,7 @@ const auth = getAuth();
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://afrofashion.site/auth/verify-seller/',
+  url: 'https://afrofashion.site/auth/verify-seller/?mode=action&oobCode=code',
   // This must be true.
   handleCodeInApp: true,
   dynamicLinkDomain: 'afrofashion.page.link'
