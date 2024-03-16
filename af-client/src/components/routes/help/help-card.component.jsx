f5f7de9c14ba706b777a4f1b5309f5cf2b850353
Gideon, I <gicodes9@gmail.com>
2024-03-16 10:40:44 +0100

    help

--- af-client/src/components/routes/help/help-card.component.jsx
+++ af-client/src/components/routes/help/help-card.component.jsx
@@ -8,12 +8,13 @@ export const HelpCard = () => {
         <p>
           Afrofashion is an African-first online platform that connect buyers to sellers, 
           offer complete e-commerce services and help small-scale african brands grow and manage their businesses. 
-          <span className="block mt-1 mb-1">Fashion-instas can take e-shopping to the next level with services from over 25 brands, 
-            1000+ unique products and up to 15 item categories to choose from.
+          <span className="block mt-1 mb-1">Fashion lovers can enjoy online shopping with services from over 25 brands, 
+            1000+ unique products and up to 15 item categories. 
           </span>
-          New and experienced vendors can both benefit from Afro Fashion branding and marketing thier products.
-          Considering cross-functionality of our web, mobile and social media platforms,
-          vendors are bound to make 5x the normal sales without worrying about customer base.
+          Vendors on the other hand, can also take online sales to the next level. 
+          New and experienced sellers can both benefit from branding and marketing thier products with business services.
+          Given the cross-functionality of our web, mobile and social media platforms,
+          vendors can make 5x the normal sales without worrying about customer base.
         </p>
       </div>
       <br/>
@@ -24,58 +25,58 @@ export const HelpCard = () => {
         <section id="products">
           <h6 className="pt-4 text-secondary"><b>Products</b></h6>
           <p>
-            If our business offers are the hook to convert a client (buyer or seller), then surely our products are the catch, right?
+            If our business offers are the hook to convert a client (buyer or seller), surely our products are the catch, right?
             <span className="block mb-2">Fashion products are self-promoting, 
               but it’s our job to set the hook right so that a customer is happy with the catch.
             </span>
-            <span className="block mb-2">You can find all active products in our <a href="/marketplace" className="link">marketplace</a>. 
-              However, products can vary widely, considering how diverse people's fashion senses can be. 
-              This is why we have grouped collections to enhance user engagement and maintain clarity in variety.
+            <span className="block mb-2">Active products are listed in our <a href="/marketplace" className="link">marketplace</a>. 
+              However, these products can vary widely, considering how diverse people's fashion senses can be. 
+              This is why we have classed products into grouped collections to enhance user engagement and maintain clarity in variety.
               <span className="block mt-2">Here are five (5) <b>classes</b> of products;</span>
             </span>
             <span className="block mb-2"><b>Categories</b>: The most distinctive class to identify or classify a product with is category. 
               There are currently over 15 product categories, ranging from accessories, bags, shoes, men to women clothing, etc.
             </span>
-             <span className="block mb-2"><b>Brands</b>: At Afrofashion, we care about commerce and originality. Since, every product has a vendor (seller), 
-              it's only natural for products to be classed by the seller. Sellers with multiple product categories can be known as <b>AF</b> brands.
+             <span className="block mb-2"><b>AF Brands</b>: At Afrofashion, we care about commerce and originality. Since, every product has a vendor (seller), 
+              it's only organic for sellers to be a major class of products. Sellers with multiple product categories can be known as <b>AF</b> brands.
             </span>
-            <span className="block mb-2"><b>Location</b>: The best services are often the simplest. We try to ease e-commerce by shortening the distance to a seamless service.
-              Users can search for vendors or brands registered within a particular location.
+            <span className="block mb-2"><b>Location</b>: The best services are often the quickest. We try to ease e-commerce by closing the distance to a seamless service.
+              Users can search for vendors or brands closeby, or in a particular location.
             </span>
-            <span className="block mb-2"><b>Featured (Hot)</b>: Items with the most hits are collated and classed in our featured products. 
+            <span className="block mb-2"><b>Featured</b> (Hot): Items with the most hits are collated and classed in our featured products. 
               These products can be very random and irrespective of the category, seller or location.
             </span>
-            <b>Featured (New)</b>: New and recently updated items are collated and classed in our featured products. 
+            <b>Featured</b> (New): New and recently updated items are collated and classed in our featured products. 
               These products can be very random and irrespective of the category, seller or location.
           </p>
         </section>
 
         <section id="services">
-    <h6 className="pt-4 text-secondary"><b>Services</b></h6>
-    <p>
-        Get ahead of the line by getting started with our services that ensure a smooth and satisfying experience for the clients.
-        Our commitment to excellence extends beyond what you buy, to why and how you buy it.
-        <span className="block mt-2 mb-2">Explore our range of services for a buyer, seller or third-party</span>
-        <span className="block mb-2"><b>Secure Solutions</b>: Afrofashion system and services are built with client and server security in mind. 
-        We have adopted various modernized tools and technology to ensure our users enjoy secure solutions.</span>
-        <span className="block mb-2"><b>Self-services</b>: Clients like to be in control, and fortunately the customer is always right.
-        Afrofashion run a system that give users limited control over user-specific actions on the platform.</span>
-        <span className="block mb-2"><b>Management</b>: In addition to self-services, Afrofashion also offer premium and express management solutions to both buyers and sellers.
-        This is ideal to meet certain user demands and preferences.</span>
-        <span className="block mb-2"><b>Customer Support</b>: Afrofashion's business model puts a user's needs first. 
-          We have dedicated customer support to address any queries or concerns promptly and efficiently.</span>
-        <span className="block mb-2"><b>Marketing</b>: There is an uncontested need for marketing in commerce. With business subscription, 
-          users are given access to a vast network of vendors and industry contacts to facilitate sales and growth.</span>
-    </p>
-</section>
-
-      </div>
-      <br/>
+          <h6 className="pt-4 text-secondary"><b>Services</b></h6>
+            <p>
+              Get ahead of the line by getting started with our services that ensure a smooth and satisfying experience for the clients.
+              Our commitment to excellence extends beyond what you buy, to why and how you buy it.
+              
+              <span className="block mt-2 mb-2"> Explore our range of services</span>
+              <span className="block mb-2"><b>Secure Solutions</b>: Afrofashion system and services are built with client and server security in mind. 
+                We have adopted various modernized tools and technology to ensure our users enjoy secure solutions.</span>
+              <span className="block mb-2"><b>Self-services</b>: Clients like to be in control, and fortunately the customer is always right.
+                Afrofashion run a system that give users limited control over user-specific actions on the platform.</span>
+              <span className="block mb-2"><b>Management</b>: In addition to self-services, Afrofashion also offer premium and express management solutions to both buyers and sellers.
+                This is ideal to meet certain user demands and preferences.</span>
+              <span className="block mb-2"><b>Customer Support</b>: Afrofashion's business model puts a user's needs first. 
+                We have dedicated customer support to address any queries or concerns promptly and efficiently.
+              </span>
+              <span className="block mb-2"><b>Marketing</b>: There is an uncontested need for marketing in e-commerce. With business subscription, 
+                users are given access to a vast network of vendors and industry contacts to facilitate sales and growth.</span>
+            </p>
+        </section>
+      </div><br/>
       
       <div id="buying">
         <h6><u>What do I need to start buying?</u></h6>
         <p>
-          Start buying on AfroFashion by signing up as a <a href="/auth/register" className="link">buyer</a>, 
+          Start buying on Afrofashion by signing up as a <a href="/auth/register" className="link">buyer</a>, 
           with email - password or by using a <a href="/auth/register#google" className="link">g-mail </a>account. 
           <span className="block mt-2 mb-2"> Users can browse products randomly, 
             shop from the <a href="/marketplace" className="link">marketplace</a>, or buy items from verified sellers for free.
@@ -95,12 +96,10 @@ export const HelpCard = () => {
             <li><u>Personalized Ads</u>: Control what you see and only see what you like</li>
           </p>
         </section>
-      </div>
-      <br />
+      </div><br/>
      
       <div id="selling">
-        <h6><u>What do I need to start selling?</u></h6>
-        
+        <h6><u>What do I need to start selling?</u></h6>        
           To start selling on Afrofashion, you will need to sign up as a <a href="/auth/register#seller" className="link">seller</a>, 
           and verify your account with email <span className="text-gray">or phone</span>. You'll be redirected to a Terms & Conditions page, 
           and then sent an email to complete an email verification.
@@ -132,8 +131,7 @@ export const HelpCard = () => {
            <li> Loyalty Packages: Cash and prize incentives are up for grabs. We are happy to reward milestone sales and motivate every business brand</li>
           </p>
         </section>
-      </div>
-      <br />
+      </div> <br/>
 
       <div id="payments">
         <h6><u>Payments on Afro Fashion</u></h6>
@@ -155,8 +153,8 @@ export const HelpCard = () => {
             What's a good business without real and stable market rates? 
           </p>
           <span className="block mt-2 mb-1">The default currency used to checkout payments on Afrofashion platforms is NGN, albeit products are listed and valued in USD. 
-              We have taken initiative to balance the spread on the exchange rates with accurate data from live markets. 
-              At the moment, our only supported currency-exchange pair is USD/NGN (US Dollar - Nigerian Naira).
+            We have taken initiative to balance the spread on the exchange rates with accurate data from live markets. 
+            At the moment, our only supported currency-exchange pair is USD/NGN (US Dollar - Nigerian Naira).
           </span>
           In Collaboration with the best payment platforms, we can easily serve our users with up-to-date rates and seamless conversion between fiat amounts.
           Checking out, purchasing products or balancing point of sales can now be in local fiat (Naira).
@@ -173,14 +171,12 @@ export const HelpCard = () => {
             <span className="block mt-2 mb-3">Check out the Pros & Compare our <a href="/subscriptions" className="link">subscriptions</a> in detail</span>
             <li>Basic Package</li>
             <li>Business Package</li>
-            <li>Premium Package</li>
-            <br/>
-            <b>Please note:</b> clicking on <span className="text-gray"><b>Subscribe Now</b></span> will initiate a payment window. 
+            <li>Premium Package</li> 
+            <br/> <b>Please note:</b> clicking on <span className="text-gray"><b>Subscribe Now</b></span> will initiate a payment window. 
               You are advised not start this process if you are not ready to pay for your subscription.
           </p>
         </section>
-      </div>
-      <br />
+      </div> <br/>
 
       <div id="shipping">
         <h6><u>Shipping with Afro Fashion</u></h6>
