1f5d618104880393abd29db8b523b71e5e9e6673
Gideon I <gicodes9@gmail.com>
2024-04-28 13:43:08 +0100

    marketplace redefine

--- af-client/src/components/assets/circle/round-circle.jsx
+++ af-client/src/components/assets/circle/round-circle.jsx
@@ -2,7 +2,7 @@ import './round-circle.styles.scss';
 
 const RoundCircleCard = ({title}) => {
   return (
-    <div className="mt-5 round-circle-card">
+    <div className="mt-1 round-circle-card">
         <br/>
         <div className='card-body'>
             <p className='v-center'>
