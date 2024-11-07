7d798908b1303cea20eb6c7e09153fc6964b48f0
Gideon I <gicodes9@gmail.com>
2024-11-07 20:10:10 +0100

    Af-server: refactor server.js

--- af-server/server.js
+++ af-server/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+require('dotenv').config();
 
-dotenv.config();
+const cors = require('cors');
+const express = require('express');
+const apiRoutes = require('./api');
+
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+app.use('/api', apiRoutes); 
 
 const PORT = process.env.PORT;
 
