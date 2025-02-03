9d6a2f5e87619ee463e5c7b8fc1510a6da4994cb
Gideon I <gicodes9@gmail.com>
2025-02-04 00:21:15 +0100

    email verification fix 2

--- af-client/src/routes/authentication/seller-auth/verification.ts
+++ af-client/src/routes/authentication/seller-auth/verification.ts
@@ -1,16 +1,25 @@
-import { sendEmailVerification } from "firebase/auth";
+import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
+
+const auth = getAuth();
+
+const actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: 'https://afrofashion.site/auth.me', // matches verify-email link used inline actionCodeSettings
+  // This must be true.
+  handleCodeInApp: true,
+  dynamicLinkDomain: 'afrofashion.page.link' // allowed on Firebase Console
+};
 
 export const sendVerification = async (email) => {
-  if (email) {
-    try {
-      await sendEmailVerification(email, {
-        url: 'https://afrofashion.site/auth.me',
-        handleCodeInApp: true,
-        dynamicLinkDomain: 'afrofashion.page.link',
-      });
-      console.log("Verification email sent!");
-    } catch (error) {
-      console.error("Error sending verification email:", error);
-    }
-  }
+  await sendSignInLinkToEmail(auth, email, actionCodeSettings)
+  .then(() => {
+    // The link was successfully sent. Inform the user
+    // Save the email locally so you don't need to ask the user for it again if they open the link on the same device...
+    window.localStorage.setItem('emailForSignIn', email);
+  })
+  .catch((error) => {
+    const errorMessage = error.message;
+    console.error(errorMessage);
+  });
 }
